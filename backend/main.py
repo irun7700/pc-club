4969efa9c4add087189f5506920fb9b98c25a4d1
Azamat <macbook@MacBook-Air-macbook-2.local>
2026-05-22 00:58:53 +0500

    Добавлен CORS для Netlify

--- backend/main.py
+++ backend/main.py
@@ -18,7 +18,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
+    allow_origins=["http://localhost:5173", "https://preeminent-meringue-a0a982.netlify.app"],
     allow_methods=["*"],
     allow_headers=["*"],
 )
